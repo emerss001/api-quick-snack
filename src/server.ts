34d450bbc790d40259446379cd05c7c5b68da790
emerss001 <emersonnevess80@gmail.com>
2025-04-12 10:58:04 -0300

    adiciona suporte a CORS e registra novas rotas no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,29 @@
 import Fastify from "fastify";
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { getProductsByCategory } from "./routes/get-products-by-category";
+import { getCategoryNamesRoute } from "./routes/get-category-names";
+import fastifyCors from "@fastify/cors";
+import { getProductById } from "./routes/get-product-by-id";
 
 const app = Fastify({
     // logger: true,
 }).withTypeProvider<ZodTypeProvider>();
+app.register(fastifyCors, {
+    origin: "*",
+});
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 // routes by application
 app.register(getProductsByCategory);
+app.register(getCategoryNamesRoute);
+app.register(getProductById);
 
-app.listen({ port: 3000 }, (err, address) => {
+app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
     if (err) {
-        app.log.error(err);
+        console.log(err);
         process.exit(1);
     }
-    app.log.info(`Server listening at ${address}`);
+    console.log(`Server listening at ${address}`);
 });
